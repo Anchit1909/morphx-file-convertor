b33b25403563dbc4e9bec1b3b43206cd7acd3be3
Anchit Sinha <anchit1909@gmail.com>
2024-01-09 01:56:44 +0530

    Changed Design of Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Dropzone from "@/components/Dropzone";
 export default function Home() {
   return (
     <main className="relative mx-auto w-full max-w-screen-lg px-2.5 md:px-20 pt-20 pb-10">
-      <div className="absolute w-[500px] h-[500px] right-[600px] -bottom-8 bg-red-500/[22%] rounded-full -z-10 blur-3xl" />
-      <div className="absolute -right-32 bottom-32 w-[500px] h-[500px] bg-purple-700/[22%] rounded-full blur-3xl" />
+        <div className="absolute w-[500px] h-[500px] right-[600px] -bottom-8 bg-red-500/[22%] rounded-full -z-10 blur-3xl" />
+        <div className="absolute -right-32 bottom-32 w-[500px] h-[500px] bg-purple-700/[22%] rounded-full blur-3xl" />
       <div className="space-y-16">
         <div className="space-y-6">
           <div className="space-y-2">
--- src/components/Navigation/Footer.tsx
+++ src/components/Navigation/Footer.tsx
@@ -2,13 +2,17 @@ function Footer() {
   return (
     <footer className="h-12 inset-x-0 top-0 z-30 w-full backdrop-blur-lg transition-all">
       <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
-        <div className="flex justify-center">
-          <p className="text-muted-foreground font-poppins text-sm md:text-base">
-            © 2023{" "}
-            <span className="text-primary bg-clip-text font-semibold">
-              Anchit Sinha.
-            </span>{" "}
-            All rights reserved
+        <div className="flex justify-center text-gray-400">
+          <p className="text-lg md:text-base">
+            A Project by{" "}
+            <a
+              className="font-semibold underline-offset-4 transition-colors hover:underline"
+              href="https://twitter.com/anchit1909"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Anchit Sinha
+            </a>
           </p>
         </div>
       </div>

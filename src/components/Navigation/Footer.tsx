5160ebc4eaabcbe7aaf6ef6bdc82fdc65fd931bd
Anchit Sinha <anchit1909@gmail.com>
2024-01-14 01:12:52 +0530

    Made mobile responsive

--- src/components/Navigation/Footer.tsx
+++ src/components/Navigation/Footer.tsx
@@ -3,7 +3,7 @@ function Footer() {
     <footer className="h-12 inset-x-0 top-0 z-30 w-full backdrop-blur-lg transition-all">
       <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
         <div className="flex justify-center text-gray-400">
-          <p className="text-lg md:text-base">
+          <p className="text-base">
             A Project by{" "}
             <a
               className="font-semibold underline-offset-4 transition-colors hover:underline"
